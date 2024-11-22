f05ab3bba3d140adba4a5452efb3d38df5322ab4
linusidom <linusidom@gmail.com>
2024-11-23 04:48:02 +0700

    updated fields

--- src/components/TarotCard.js
+++ src/components/TarotCard.js
@@ -1,12 +1,11 @@
 export const TarotCard = ({card, reversed, cardText}) => {
     // console.log(card, reversed, cardText)
-    const url = 'tarottool/assets'
     return(
         <div>
             {reversed ?
-                <img src={`${url}/${card}.jpg`} alt="" srcSet="" className='reversed'/>
+                <img src={`assets/${card}.jpg`} alt="" srcSet="" className='reversed'/>
                 :
-                <img src={`${url}/${card}.jpg`} alt="" srcSet="" />
+                <img src={`assets/${card}.jpg`} alt="" srcSet="" />
             }
             <p className="card-description">{cardText}</p>
         </div>
